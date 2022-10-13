8ccd5ef30f3ccc681a170f76147decba9b97b10e
Diopranata01 <bagusdio01@gmail.com>
2022-10-14 00:52:20 +0800

    New commit

--- unit-testing/src/App.js
+++ unit-testing/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+// import NameForm from './FormCoding'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      {/* <NameForm/> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
